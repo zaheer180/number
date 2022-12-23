f1f56c66160c834ef53953245780bc83ba2c42af
zaheer180 <81247008+zaheer180@users.noreply.github.com>
2022-12-23 21:59:05 +0000

    C:\Users\zahee\OneDrive\Desktop\javascript\MIK project\scrimba\cards\ui coursecard

---
+++ basiccalculator/calculator.js
@@ -0,0 +1,20 @@
+ let result  = document.getElementById("inputext");
+ let calculate =(number)=>{
+    result.value+= number;
+ }
+ let Result =()=>{
+  try{
+    result.value=eval(result.value)
+  }
+  catch(err){
+  alert("enter avalid operation")
+  
+  
+  }
+ }
+ function clr(){
+    result.value = "";
+ }
+ function del(){
+    result.value = result.value.slice(0,-1);
+ }
